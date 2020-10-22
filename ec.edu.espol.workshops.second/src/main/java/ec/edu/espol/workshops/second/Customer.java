799dc932a49bac3a69e37a0bf76f746498b1cf9f
miguelquo <miguelps95@gmail.com>
2020-10-22 18:24:35 -0500

    add insurance functionality

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Customer.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Customer.java
@@ -4,15 +4,34 @@ public class Customer {
 	private int age;
 	private char sex;
 	private boolean isMarried;
+	private boolean hasLincense = false;
 	
 	public Customer(int age, char sex, boolean isMarried) {
 		this.age = age;
 		this.sex = sex;
 		this.isMarried = isMarried;
 	}
-
+	
+	public boolean canBuyInsurance() {
+		if (!this.hasLincense) {
+			return false;
+		}
+		if(this.age >= 80) {
+			return false;
+		}
+		return true;
+	}
+	
 	public int getAge() {
 		return age;
+	}
+	
+	public boolean hasLincense() {
+		return this.hasLincense;
+	}
+	
+	public void setHasLicense(boolean has) {
+		this.hasLincense = has;
 	}
 
 	public void setAge(int age) {
