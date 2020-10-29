45d9dd643114bba36a4cdd17e5629d8bd1f4058a
miguelquo <miguelps95@gmail.com>
2020-10-29 18:18:02 -0500

    merge

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Customer.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Customer.java
@@ -1,57 +1,56 @@
 package ec.edu.espol.workshops.second;
 
 public class Customer {
-    private int age;
-    private char sex;
-    private boolean isMarried;
-    private boolean hasLincense = false;
+  private int age;
+  private char sex;
+  private boolean isMarried;
+  private boolean hasLincense = false;
 
-    public Customer(int age, char sex, boolean isMarried) {
-	this.age = age;
-	this.sex = sex;
-	this.isMarried = isMarried;
+  public Customer(int age, char sex, boolean isMarried) {
+    this.age = age;
+    this.sex = sex;
+    this.isMarried = isMarried;
+  }
+
+  public boolean canBuyInsurance() {
+    if (!this.hasLincense) {
+      return false;
     }
-
-    public boolean canBuyInsurance() {
-	if (!this.hasLincense) {
-	    return false;
-	}
-	if (this.age >= 80) {
-	    return false;
-	}
-	return true;
+    if (this.age >= 80) {
+      return false;
     }
+    return true;
+  }
 
-    public int getAge() {
-	return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public boolean hasLincense() {
-	return this.hasLincense;
-    }
+  public boolean hasLincense() {
+    return this.hasLincense;
+  }
 
-    public void setHasLicense(boolean has) {
-	this.hasLincense = has;
-    }
+  public void setHasLicense(boolean has) {
+    this.hasLincense = has;
+  }
 
-    public void setAge(int age) {
-	this.age = age;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public char getSex() {
-	return sex;
-    }
+  public char getSex() {
+    return sex;
+  }
 
-    public void setSex(char sex) {
-	this.sex = sex;
-    }
+  public void setSex(char sex) {
+    this.sex = sex;
+  }
 
-    public boolean isMarried() {
-	return isMarried;
-    }
+  public boolean isMarried() {
+    return isMarried;
+  }
 
-    public void setIsMarried(boolean isMarried) {
-	this.isMarried = isMarried;
-    }
-
+  public void setIsMarried(boolean isMarried) {
+    this.isMarried = isMarried;
+  }
 }
