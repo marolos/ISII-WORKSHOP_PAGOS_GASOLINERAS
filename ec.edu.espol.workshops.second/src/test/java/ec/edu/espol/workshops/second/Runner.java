e3e68d2f9e19571a6cffb2a75a61f26ed72d55b0
miguelquo <miguelps95@gmail.com>
2020-11-12 18:32:06 -0500

    add runner jUnit

---
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/Runner.java
@@ -0,0 +1,10 @@
+package ec.edu.espol.workshops.second;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {​​​​​"pretty","html: build/reports/cucumber-report.html"}​​​​​, monochrome = true)
+public class Runner{
+    
+}​​​​​
